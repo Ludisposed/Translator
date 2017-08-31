e18d9bbfce6a42b55def9ac66a7d496906c6df18
Qin <aries.duanmu@gmail.com>
2017-08-31 22:31:39 +0800

    little meaning less update

--- my_own_translator.py
+++ my_own_translator.py
@@ -1,26 +1,59 @@
 # -*- coding: utf-8 -*-
+import googletrans
 from googletrans import Translator
+
 import os
-translator = Translator()
 FILENAME = 'translations.txt'
+LANGCODES = dict(map(reversed, googletrans.LANGUAGES.items()))
+class MyTranslator(object):
 
-def translate_string(inp):
-    translated_part = translator.translate(inp, dest='zh-CN')
-    if os.path.exists(FILENAME):
-        append_write = 'a' # append if already exists
-    else:
-        append_write = 'w' # make a new file if not
+    def __init__(self):
+        self.translator = Translator(service_urls=['translate.google.cn'])
+        self.filename = 'translations.txt'
 
-    try:
-        f = open(FILENAME,append_write)
-        f.write('Origin: %s\n' % (translated_part.origin.encode('utf-8')))
-        f.write('Translation: %s\n\n' % (translated_part.text.encode('utf-8')))
-        f.close()
-        
-    except Exception, e:
-        print e
-        f.close()
+    def translate_chinese_to_english(self, inp):
+        translate_string(self,inp, dest = 'en', src='zh-cn')
+    def translate_english_to_chinese(self, inp):
+        translate_string(self, inp, dest='zh-cn',src='en')
+
+    def translate_string(self, inp, dest='en', src = 'auto'):
+        dest = self.dest_language(dest)
+        if src != 'auto':
+            src = self.dest_language(dest)
+        translated_part = self.translator.translate(inp, dest = dest, src = src)
+        origin = translated_part.origin.encode('utf-8')
+        text = translated_part.text.encode('utf-8')
+        print('*** Translated: '+text)
+        if os.path.exists(FILENAME):
+            append_write = 'a' # append if already exists
+        else:
+            append_write = 'w' # make a new file if not
+
+        try:
+            f = open(self.filename,append_write)
+            f.write('Origin: %s\n' % (origin))
+            f.write('Translation: %s\n\n' % (text))
+            f.close()
+
+        except Exception as e:
+            print(e)
+            f.close()
+    def dest_language(self, language):
+        if language in googletrans.LANGUAGES:
+            return language
+        language = language.lower()
+        d = self.translator.detect(language).lang
+        if d == 'en' and language != 'english':
+            if language == 'chinese':
+                return 'zh-CN'
+            return LANGCODES[language]
+        return d
+
+
+
 
 if __name__ == "__main__":
-    inp = raw_input('What do you want to translate: ')
-    translate_string(inp)
+    transtor = MyTranslator()
+    dest = raw_input('*** Which language do you want to translated to: ')
+    inp = raw_input('*** What do you want to translate: ')
+    transtor.translate_string(inp,dest)
